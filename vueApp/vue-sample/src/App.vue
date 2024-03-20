c955541d5b70cdcfb815fdb5138e847dcb96989b
unagiya <big.stone.net@gmail.com>
2024-03-20 20:05:49 +0900

    todoList save localStorage

--- vueApp/vue-sample/src/App.vue
+++ vueApp/vue-sample/src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import TheFooter from "@/components/TheFooter.vue";
+import MainTodo from "@/components/MainTodo.vue";
 </script>
 
 <template>
   <div class="wrap">
     <TheHeader />
-    <mina class="main">メイン</mina>
+    <main class="main">
+      <MainTodo />
+    </main>
     <TheFooter />
   </div>
 </template>
---
+++ vueApp/vue-sample/src/components/MainTodo.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {ref} from "vue"
+
+const todoRef = ref("");
+const todoListRef = ref([]);
+const addTodo = () => {
+  const id = new Date().getTime();
+  todoListRef.value.push({id: id, task: todoRef.value});
+  localStorage.todoList = JSON.stringify(todoListRef.value);
+
+  todoRef.value = "";
+};
+</script>
+
+<template>
+  <div class="box_input">
+    <input
+        type="text"
+        class="todo_input"
+        v-model="todoRef"
+        placeholder="+ TODOを入力"
+    >
+    <button class="btn" @click="addTodo">追加</button>
+  </div>
+</template>
+
+<style scoped>
+.box_input{
+  margin-top: 20px;
+}
+
+.todo_input{
+  width: 300px;
+  margin-right: 8px;
+  padding: 8px;
+  font-size: 18px;
+  border: 1px solid #aaa;
+  border-radius:6px;
+}
+
+.btn{
+  padding: 8px;
+  background-color: #03a9f4;
+  border-radius: 6px;
+  color: #ffffff;
+  text-align: center;
+  font-size: 14px;
+}
+</style>
