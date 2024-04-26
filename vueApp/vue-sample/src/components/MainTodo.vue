c2193caf29045d238566fee6f65af2827c08aa3a
unagiya <big.stone.net@gmail.com>
2024-04-26 22:43:36 +0900

    v-forの基本動作とその実装について追加

--- vueApp/vue-sample/src/components/MainTodo.vue
+++ vueApp/vue-sample/src/components/MainTodo.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import {ref} from "vue"
 
+
 const todoRef = ref("");
 const todoListRef = ref([]);
+const ls = localStorage.todoList;
+todoListRef.value = ls ? JSON.parse(ls) : [];
+
 const addTodo = () => {
   const id = new Date().getTime();
   todoListRef.value.push({id: id, task: todoRef.value});
@@ -22,9 +26,53 @@ const addTodo = () => {
     >
     <button class="btn" @click="addTodo">追加</button>
   </div>
+
+  <div class="box_list">
+    <div class="todo_list" v-for="todo in todoListRef" :key="todo.id">
+      <div class="todo">
+        <input type="checkbox" class="check">
+        <label>{{ todo.task }}</label>
+      </div>
+      <div class="btns">
+        <button class="btn green">編</button>
+        <button class="btn pink">削</button>
+      </div>
+    </div>
+  </div>
+
+  <div v-for="(example,index) in todoExample" :key="index">
+    <p>{{ index }}.{{ example }}</p>
+  </div>
 </template>
 
 <style scoped>
+
+.box_list{
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+
+.todo_list{
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.todo{
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 12px;
+  width: 300px;
+}
+
+.check{
+  border: 1px solid red;
+  transform: scale(1.6);
+  margin: 0 16px 2px 6px;
+}
+
 .box_input{
   margin-top: 20px;
 }
@@ -46,4 +94,19 @@ const addTodo = () => {
   text-align: center;
   font-size: 14px;
 }
+
+.btns {
+  display: flex;
+  gap: 4px;
+}
+
+.green{
+  background-color: #00c853;
+}
+
+.pink{
+  background-color: #ff4081;
+}
+
+
 </style>
